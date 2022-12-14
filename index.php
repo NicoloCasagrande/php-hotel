0baf528cc50eddcfa1e3544951391bd43a68c4b1
NicoloCasagrande <nicolo.casagrande7@gmail.com>
2022-12-14 20:48:11 +0100

    boooo

--- index.php
+++ index.php
@@ -70,6 +70,8 @@
         <button type="submit">Invia</button>
     </form>
     <?php foreach ($hotels as $key => $value){ ?>
+        <?php if($value['parking'] || $value['vote']){
+            ?>
         <div class="row">
             <div class="col">
                 <span><?php echo $value['name']?></span>
@@ -93,6 +95,7 @@
                 <span><?php echo $value['distance_to_center']?></span>
             </div>
         </div>
+        <?php } ?>
     <?php } ?>
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
